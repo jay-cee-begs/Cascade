4f43a332fc720765248f0c9f6b3bdce6197bd69c
JC_home <jcbegs@gmail.com>
2025-02-01 17:26:48 +0100

    Fix Cascade setup.py for suite2p compatibility

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ setup(
     author="Peter Rupprecht",
     author_email="",
     packages=find_packages(),
-    python_requires=">=3.7, <3.8",
+    python_requires=">=3.7, <3.9",
     install_requires=[
-        "numpy",
+        "numpy==1.21.6",
         "scipy",
         "matplotlib",
-        "tensorflow==2.3",  # pip install CPU and GPU tensorflow
-        "keras==2.3.1",
+        "tensorflow==2.11.0",  # pip install CPU and GPU tensorflow
+        "keras==2.11.0",
         "h5py",
         "seaborn",
         "ruamel.yaml",
